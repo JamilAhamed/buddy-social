c08c91b13bba96b18535afa21baa1c66ddc5110e
Your Name <you@example.com>
2021-03-08 18:14:53 +0600

    social buddy

---
+++ src/Components/Home/Post/PostDetails/PostDetails.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import Comment from "../../../Comment/Comment"
+
+const PostDetails = () => {
+    const{id} = useParams();
+    console.log(id);
+    const [post, setPost] = useState({});
+    const [comment, setComment] = useState([]);
+
+    useEffect(() =>{
+        const url =`https://jsonplaceholder.typicode.com/posts/${id}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setPost(data));
+    }, [])
+
+    useEffect ( () =>{
+        const url =`https://jsonplaceholder.typicode.com/comments?postid=${id}`
+        fetch(url)
+        .then(res=> res.json())
+        .then(data => setComment(data));
+    },[])
+    return (
+        <div>
+            <h3>This is post details: {id}</h3>
+            <p>User Posted:{post.id}</p>
+            <p>Post body: {post.body}</p>
+            <p>title: {post.title}</p>
+            <p>Number of Comments: {comment.length}</p>
+            {
+                comment.map(comment => <Comment comment={comment}></Comment>)
+            }
+        </div>
+    );
+};
+
+export default PostDetails;
